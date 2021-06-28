e76eb6f5812182c46bd631203a9a82c299e45c62
Fábio L <fflucas@hotmail.com>
2021-06-28 07:37:18 -0300

    Commit de alteração para revisão manual

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Olá mundo! Este é meu primeiro pipeline de entrega contínua'));
+app.get('/', (req, res) => res.send('Olá mundo! Este é meu primeiro pipeline de entrega contínua, porém agora ele necessita de revisão para ser publicado'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
